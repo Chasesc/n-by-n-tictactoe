b45270c02d1e727987e22762acc3f939d46a9f79
Chase Schelthoff <schelthoff.chase@gmail.com>
2016-04-20 16:58:29 -0400

    Started implementing minimax for AI_DIFFICULTY_HARD

--- tictactoe/game.py
+++ tictactoe/game.py
@@ -22,6 +22,7 @@ class Game(object):
             if Const.AI_DIFFICULTY == Const.AI_DIFFICULTY_EASY:
                 self.ai = tictactoeAgent.RandomAgent()
             elif Const.AI_DIFFICULTY == Const.AI_DIFFICULTY_HARD:
+                print "Warning: AI_DIFFICULTY_HARD is not yet implemented."
                 self.ai = tictactoeAgent.MiniMaxAgent()
             else: raise Exception("AI_DIFFICULTY in constants.py set to an invalid option.")
 
@@ -30,16 +31,16 @@ class Game(object):
                 if event.type == pygame.QUIT:
                     self.running = False                   
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    if not self.in_menu and Rules.winner is None:
+                    if not self.in_menu and Rules.winner is None and not self.game_state.is_ai_turn():
                         self.game_state.place(get_loc_from_mouse(event.pos))
-                        print self.game_state.get_current_player()
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_r: 
                         Game(self.game_state.is_against_ai()).run() # Restart the game.
 
             if Rules.is_terminal_state(self.game_state.get_board()):
                 # TODO: Ask the user if they want to play again.
-                         
+                
+                Rules.set_winners(self.game_state.get_board())                         
                 graphics.draw_game(self.game_state.get_board(), Rules.winner_indices)
             else:
                 if self.in_menu: graphics.draw_menu()
@@ -64,6 +65,10 @@ class GameState(object):
         self.__player_two = ['X', 'O'][not rand]
 
         self.__current_turn = self.__player_one if rand == 0 else self.__player_two # 'X' goes first
+
+        Rules.player_piece = self.__player_one
+        Rules.ai_piece = self.__player_two if against_ai else None
+        
 
     def is_against_ai(self):
         return self.__against_ai
@@ -92,7 +97,10 @@ class GameState(object):
         return str
 
 
-class Rules(object):
+class Rules(object):   
+
+    player_piece = None
+    ai_piece = None
 
     winner = None
     winner_indices = []
@@ -132,9 +140,7 @@ class Rules(object):
 
     @staticmethod
     def has_won(board, player):
-        won = Rules.diag_win(board, player) or Rules.col_win(board, player) or Rules.row_win(board, player)
-        if won: Rules.winner = player
-        return won
+        return Rules.diag_win(board, player) or Rules.col_win(board, player) or Rules.row_win(board, player)
 
     @staticmethod
     def either_won(board):
@@ -144,9 +150,15 @@ class Rules(object):
     def is_tie(board):
         return len(Rules.avaliable_moves(board)) == 0
 
+
     @staticmethod
     def is_terminal_state(board):
         return Rules.either_won(board) or Rules.is_tie(board)
+
+    @staticmethod
+    def set_winners(board):
+        if Rules.has_won(board, 'X'): Rules.winner = 'X'
+        elif Rules.has_won(board, 'O'): Rules.winner = 'O'
 
 
 ''' Returns the index to the board from a mouse_location.  For example, clicking in
@@ -155,8 +167,6 @@ def get_loc_from_mouse(mouse_location):
     # box_width and box_height are the width and height of a single game square.
     box_width = Const.WINDOW_WIDTH // Const.BOARD_N
     box_height = Const.WINDOW_HEIGHT // Const.BOARD_N
-
-    print box_height
 
     x = mouse_location[0]
     y = mouse_location[1]
--- tictactoe/tictactoeAgent.py
+++ tictactoe/tictactoeAgent.py
@@ -22,19 +22,74 @@ class RandomAgent(Agent):
         if len(aval_moves) != 0: return choice(aval_moves)
         else: return 0
 
+# TODO: Implement Alpha–beta pruning for improved performance
 ''' Agent that picks the move that maximizes utility assuming the opponent is playing to minimize utility. '''
 class MiniMaxAgent(Agent):
 
+    debug = False # TODO: Remove this once complete
+
     def get_action(self, state):
         if state is None: raise ValueError("MiniMaxAgent - get_state: state should not be None!")
-        return 0 # Do nothing for now.
+        utility, action = self.max_value(state, 4)
+        return action
 
-    #TODO: implement this.
-    def __mini_max(self, state, max):
-        if Rules.is_terminal_state(state):
-            pass
+    def max_value(self, state, depth):
+        if Rules.is_terminal_state(state) or depth == 0: # terminal-test
+            return (self.utility(state, Rules.ai_piece), 0)
 
-        if max:
-            pass
-        else: # min
-            pass
+        avaliable_actions = Rules.avaliable_moves(state)
+        maximum = -float('inf')
+        best_action = None        
+
+        for action in avaliable_actions:
+            new_state = self.result(state, action, Rules.ai_piece)
+            new_max = self.min_value(new_state, depth - 1)[0]
+            if maximum <= new_max:
+                maximum, best_action = new_max, action
+
+        if self.debug:
+            print "max: (1/2) ", avaliable_actions, " ", state
+            print "max: (2/2) ", (maximum, best_action), " ", depth
+            raw_input()
+
+        return (maximum, best_action)
+
+    def min_value(self, state, depth):
+        if Rules.is_terminal_state(state) or depth == 0: # terminal-test
+            return (self.utility(state, Rules.player_piece), 0)
+
+        avaliable_actions = Rules.avaliable_moves(state)
+        minimum = float('inf')
+        best_action = None
+
+        for action in avaliable_actions:
+            new_state = self.result(state, action, Rules.player_piece)
+            new_min = self.max_value(new_state, depth - 1)[0]
+            if minimum >= new_min:
+                minimum, best_action = new_min, action
+
+        if self.debug:
+            print "min: (1/2) ", avaliable_actions, " ", state
+            print "min: (2/2) ", (minimum, best_action), " ", depth
+            raw_input()
+       
+        return (minimum, best_action)
+
+    # returns a new state which is the result of a given action and piece
+    def result(self, state, action, piece):
+        # make a new copy instead of using the reference and changing the actual gameboard
+        new_state = [p for p in state] 
+        new_state[action] = piece
+        return new_state
+
+
+    # returns the utility of a state of a given state
+    def utility(self, state, piece):
+        other_piece = 'X' if piece == 'O' else 'O'
+
+        if Rules.has_won(state, piece):
+            return 1
+        elif Rules.has_won(state, other_piece):
+            return -1
+        else:
+            return 0
